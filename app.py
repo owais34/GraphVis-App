18d02df00e92cf83617ce46dd9724c5382183b36
owais34 <owaishunter78@gmail.com>
2020-09-02 09:56:58 +0530

    created virtualenv using pipenv

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask ,render_template,request,redirect,url_for,flash,jsonify
 import requests
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 app.config['DEBUG']=True
 
 # Here we will have global graph data structure
@@ -16,14 +18,17 @@ global_graph={"node":[],"edges":[]}
 
 @app.route('/',methods=['GET'])
 def root():
-    return "Hello world"
+    return jsonify(global_graph)
 
 @app.route('/addNode',methods=['GET','POST'])
 def addNode():
     # Here we add a node and give it a default color
-    global_graph["node"].append([len(global_graph["node"]),nodeColor["default"]])
-    print(global_graph)
+    x=int(request.args.get('x',None))
+    y=int(request.args.get('y',None))
+    global_graph["node"].append([len(global_graph["node"]),nodeColor["default"],x,y])
     return jsonify(global_graph)
+
+# def checkValidEdge(source,sink):
 
 # To add an edge we send request to /addEdge?source=&&sink=
 @app.route('/addEdge',methods=['POST','GET'])
@@ -32,8 +37,8 @@ def addEdge():
     sink=int(request.args.get('sink',None))
     #rest of functional part
     if source!=None and sink!=None:
-        tup1=(source,sink)
-        tup2=(sink,source)
+        tup1=[source,sink,edgeColor['default']]
+        tup2=[sink,source,edgeColor['default']]
         if tup1 not in global_graph["edges"] and tup2 not in global_graph["edges"]:
             if sink>=0 and sink<len(global_graph["node"]) and source>=0 and source<len(global_graph["node"]) and source!=sink:
 
